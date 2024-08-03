64d4cca84e741a812f11ccb4b977bad142c27848
Nico <nicopasquariello@gmail.com>
2024-08-03 13:50:29 -0600

    can add products to cart, working on decrementing quantities.

--- src/features/shop/ProductDetails.js
+++ src/features/shop/ProductDetails.js
@@ -3,9 +3,12 @@ import { useNavigate, useParams } from "react-router";
 import images from '../../images/images'
 import { Button, Typography } from "@mui/material";
 import LoremIpsum from "react-lorem-ipsum";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setCart } from "../../slices/backendSlice";
 
 export default function ProductDetails() {
+  const cart = useSelector(state => state.backend.cart);
+  const dispatch = useDispatch()
   const products = useSelector(state => state.backend.products)
   const [currentProduct, setCurrentProduct] = useState({})
   const [count, setCount] = useState(0)
@@ -13,14 +16,14 @@ export default function ProductDetails() {
   const navigate = useNavigate()
   useEffect(() => {
     products.map(p => {
-      if (p.id === parseInt(id)){
+      if (p.id === parseInt(id)) {
         images.map(image => {
-          if (image.includes(p.image_path)) {            
-            setCurrentProduct({...p, image: image})
+          if (image.includes(p.image_path)) {
+            setCurrentProduct({ ...p, image: image })
           }
           return null
         })
-      } 
+      }
     })
 
     return () => {
@@ -38,14 +41,49 @@ export default function ProductDetails() {
     }
   }
 
-  function submit(){
-    if (count !== 0){
-      console.log({item:currentProduct.product_name, price:currentProduct.price, type:currentProduct.product_type, quantity:count})
+  function getDifference(num1, num2) {
+    if (num1 > num2) {
+      return num1 - num2
     } else {
-      console.log({error: 'quantity must be greater than 0'})
+      return num2 - num1
     }
   }
 
+  function updateItem(item) {
+    const add = getDifference(item.quantity, count)
+
+    const itemCopy = { ...item }
+    itemCopy.quantity += add
+
+    const cartCopy = [...cart]
+
+    const updatedCart = cartCopy.map(item => {
+      if (item.id === itemCopy.id){
+        return item = itemCopy
+      }
+      return item
+    })
+
+    dispatch(setCart(updatedCart))
+  }
+
+  function submit() {
+    const addProduct = { id: currentProduct.id, item: currentProduct.product_name, price: currentProduct.price, type: currentProduct.product_type, quantity: count }
+    
+    const inCart = cart.filter(item => item.id === currentProduct.id)[0]
+  
+    if (inCart?.id) {
+      updateItem(inCart)
+    } else if (cart.length === 0 && count > 0) {
+      dispatch(setCart([...cart, addProduct]))
+    } else if (!inCart?.id && count > 0){
+      dispatch(setCart([...cart, addProduct]))
+    } else if (count === 0){
+      console.log({error:'Quantity Must be Greater Than 0'})
+    }
+    
+  }
+  console.log('cart', cart)
   return (
     <div className="productDetailsDiv">
       <p className="goBackButton" onClick={() => navigate(-1)}> {'<<'}BACK TO SHOP</p>
